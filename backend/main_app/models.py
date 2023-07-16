31e4410a6a8380eb5002923eca57f1b2edf3fb61
Micole Atkins <micoleatkins@gmail.com>
2023-07-16 16:53:52 -0500

    cart

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=150)
-    price = models.IntegerField(null="False", blank="False")
+    price = models.IntegerField(default=0, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -28,20 +28,13 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='items')
-    order = models.ForeignKey(Order, on_delete=models.CASCADE,
-                              related_name="items")
-    quantity = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('main_app.Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product.name
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'order_id': self.id})
-
-    def get_cost(self):
-        return self.product.price * self.quantity
+        return f'{self.user.username} - {self.product.name}'
 
 
 class Customer(models.Model):
